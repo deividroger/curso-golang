461a713f7bdbc903784b3a509d553965222f8fb5
= <=>
2019-03-04 17:10:17 -0300

    Trabalhando com pacotes

---
+++ pacote/reuso/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/deividroger/area"
+)
+
+func main() {
+
+	fmt.Println(area.Circ(6.0))
+
+	fmt.Println(area.Rect(5.0, 2.0))
+}
---
+++ pacote/usandolib/usandolib.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/deividroger/goarea"
+)
+
+func main() {
+
+	fmt.Println(goarea.Circ(4.0))
+
+}
