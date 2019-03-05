af41ed21dd95323d13f30cbbb6bc5b169ef48f2b
= <=>
2019-03-05 01:28:48 -0300

    Adicionado exercicios com http

---
+++ http/dinamico/dinamico.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func horaCerta(w http.ResponseWriter, r *http.Request) {
+	s := time.Now().Format("02/01/2006 03:04:05")
+
+	fmt.Fprintf(w, "<h1>A hora certa: %s</h1>", s)
+}
+
+func main() {
+	http.HandleFunc("/horacerta", horaCerta)
+
+	log.Println("Executando...")
+
+	log.Fatal(http.ListenAndServe(":3000", nil))
+}
